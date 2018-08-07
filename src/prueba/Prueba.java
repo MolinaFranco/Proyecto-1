95d810f0730fc5a07ec53c3693259e8db88833e2
MolinaFranco <franco13mm@gmail.com>
2018-08-07 08:16:11 -0300

    Actualizo sacando todo de la carpeta secundaria

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,20 @@
+package prueba;
+
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println(Runtime.getRuntime().freeMemory());
+
+        System.out.println("-----------------------------------------------------------------");
+        
+        System.out.println("CPU usage (%):     "+ "%");
+        System.out.println("Memory    (total): "+ "MB");
+        System.out.println("Memory    (free):  "+ "MB");
+        System.out.println("Disk      (total): "+ "MB");
+        System.out.println("Disk      (free):  "+ "MB");
+    }
+    
+}
