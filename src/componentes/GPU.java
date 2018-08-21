8c481cb6091d59e3c251a19f7a79f2da992ee3a1
EbnerLautaro <lautiebner@gmail.com>
2018-08-21 10:06:28 -0300

    GPU y borro una clase que no hacia falta

--- src/componentes/GPU.java
+++ src/componentes/GPU.java
@@ -1,13 +1,39 @@
 package componentes;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class GPU {
-    
-    
+
     public static void main(String[] args) {
-    
-    
-    
-    
-    }
+        /* Se necesita instalar glxinfo, sudo apt install mesa-utils */
+        
+        System.out.println("Modelo..............: " + execute("echo "
+                + "$(glxinfo |grep \"Device:\" | cut -d \":\" -f2) | cut -d \"(\" -f 1,2"));
+        
+        System.out.println("Fabricante..........: " + execute("echo "
+                + "$(glxinfo |grep \"Vendor:\" | cut -d \":\" -f2) | cut -d \"(\" -f1"));
+        
+        System.out.println("Memoria de Video....: " + execute("echo "
+                + "$(glxinfo |grep \"Video memory:\" | cut -d \":\" -f2)"));
+        
+        System.out.println("Vercion.............: " + execute("echo "
+                + "$(glxinfo |grep \"Version\" | cut -d \":\" -f2)"));
+        
+    }    
+    public static String execute(String input) {
+        String[] cmd = {"/bin/bash", "-c", input};
+        String output = null;
+
+        try {
+            Process exec = Runtime.getRuntime().exec(cmd);
+            BufferedReader r = new BufferedReader(new InputStreamReader(exec.getInputStream()));
+            output = r.readLine();
+
+        } catch (Exception e) {
+        }
+        return output;
+
+    } 
+
 }
